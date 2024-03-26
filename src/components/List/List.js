bb5432adee72fabe8fc0cc22cf02a527ed723a2c
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-03-26 23:04:25 +0100

    Create basic Components and include global styles

---
+++ src/components/List/List.js
@@ -0,0 +1,26 @@
+const List = () => {
+  return (
+    <div>
+      <header>
+        Things to do <span>soon</span>
+      </header>
+      <p> Interesting things I want to check out</p>
+      <article>
+        <h1>Books</h1>
+      </article>
+      <article>
+        <h1>Movies</h1>
+      </article>
+      <article>
+        <h1>Games</h1>
+      </article>
+      {/* <ul>
+            <li>Learn React</li>
+            <li>Code React</li>
+            <li>Refactor React</li>
+        </ul> */}
+    </div>
+  );
+};
+
+export default List;
