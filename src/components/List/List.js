fda59cc66c67b292fa6f5a09415e9525fe0c9d60
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-03-26 23:29:43 +0100

    Create component styles

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,19 +1,28 @@
+import styles from "./List.module.scss";
+
 const List = () => {
   return (
-    <div>
-      <header>
-        Things to do <span>soon</span>
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>
+          Things to do <span>soon</span>
+        </h2>
       </header>
-      <p> Interesting things I want to check out</p>
-      <article>
-        <h1>Books</h1>
-      </article>
-      <article>
-        <h1>Movies</h1>
-      </article>
-      <article>
-        <h1>Games</h1>
-      </article>
+      <p className={styles.description}>
+        {" "}
+        Interesting things I want to check out
+      </p>
+      <section className={styles.columns}>
+        <article>
+          <h1 className={styles.title}>Books</h1>
+        </article>
+        <article>
+          <h1 className={styles.title}>Movies</h1>
+        </article>
+        <article>
+          <h1 className={styles.title}>Games</h1>
+        </article>
+      </section>
       {/* <ul>
             <li>Learn React</li>
             <li>Code React</li>
