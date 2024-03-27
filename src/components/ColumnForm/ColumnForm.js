ed04c1c5829eaeae9d12bfbcad939550665967b1
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-03-27 21:55:25 +0100

    Add html label functionality to ColumnForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,18 +2,23 @@ import styles from "./ColumnForm.module.scss";
 
 import TextInput from "../TextInput/TextInput.js";
 import Button from "../Button/Button.js";
+import { nanoid } from "nanoid";
+
+const id1 = nanoid();
+const id2 = nanoid();
 
 const ColumnForm = function (props) {
   return (
     <form className={styles.form} onSubmit={props.handleSubmit}>
-      <span>Title:</span>
+      <label htmlFor={id1}>Title:</label>
       <TextInput
         type="text"
+        id={id1}
         value={props.title}
         onChange={props.onTitleChange}
       />
-      <span>Icon:</span>
-      <TextInput value={props.icon} onChange={props.onIconChange} />
+      <label htmlFor={id2}>Icon:</label>
+      <TextInput value={props.icon} id={id2} onChange={props.onIconChange} />
       <Button>Add column</Button>
     </form>
   );
--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 const SearchForm = (props) => {
   return (
     <input
+      id={props.id}
       className={styles.input}
       type="text"
       placeholder={props.placeholder}
