939e13dda2012adde7b04b169df37937b258ef9a
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-04-03 21:45:03 +0200

    Add redux store to project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 import "./styles/normalize.scss";
 import "./styles/global.scss";
 // import fontawesome from "node_modules/fontawesome";
@@ -10,7 +13,9 @@ import "font-awesome/css/font-awesome.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
