b168ae848e367c6f661d53e646d8df0eb94d0e7e
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-04-14 18:50:29 +0200

    Add Card favorite and remove buttons

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,21 @@
 import styles from "./Card.module.scss";
 
 const Card = (props) => {
-  const element = <li className={styles.card}>{props.title}</li>;
+  const isFavorite = true;
+
+  const element = (
+    <li className={styles.card}>
+      <span>{props.title}</span>
+      <div className={styles.controls}>
+        <i
+          className={`${styles.button} ${
+            isFavorite ? styles.isFavorite : ""
+          } fa fa-star-o`}
+        ></i>
+        <i className={`${styles.button} fa fa-trash`}></i>
+      </div>
+    </li>
+  );
 
   return element;
 };
